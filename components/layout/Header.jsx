059044d31a705b26ca925da455129a6396a33427
Kimbangul <65225446+Kimbangul@users.noreply.github.com>
2023-01-11 23:15:34 +0900

    style: header add

---
+++ components/layout/Header.jsx
@@ -0,0 +1,43 @@
+import Link from 'next/link';
+import menu from 'data/menu.json';
+import app from 'data/app.json';
+
+// COMPONENT main component
+const Header = () => {
+  return (
+    <header className='Header'>
+      <HeaderTitle />
+      <HeaderMenu />
+    </header>
+  );
+};
+
+// COMPONENT header title
+const HeaderTitle = (props) => {
+  return (
+    <div className='Header__title'>
+      <Link href={props.link || '/'}>{props.title || app.title}</Link>
+    </div>
+  );
+};
+
+// COMPONENT header menu list
+const HeaderMenu = () => {
+  return (
+    <nav className='Header__menu'>
+      {menu.length > 0 && (
+        <ul className='Header__menu-list'>
+          {menu.map((el) => {
+            return (
+              <li className='Header__menu-item'>
+                <Link href={el.link}>{el.title}</Link>
+              </li>
+            );
+          })}
+        </ul>
+      )}
+    </nav>
+  );
+};
+
+export default Header;
