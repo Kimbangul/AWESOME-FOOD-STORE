914bf4e4463d8204bf9d33a574f079ab8211db11
Kimbangul <65225446+Kimbangul@users.noreply.github.com>
2023-01-12 21:26:29 +0900

    chore: install sass

--- next.config.js
+++ next.config.js
@@ -1,11 +1,16 @@
 /** @type {import('next').NextConfig} */
+const path = require('path');
+
 const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: [
       "s3.ap-northeast-2.amazonaws.com",
     ]
-  }
+  },
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
 }
 
 module.exports = nextConfig
