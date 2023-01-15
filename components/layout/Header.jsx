375e6cc1e8742152a5385c141ca6093bb5df927d
Kimbangul <65225446+Kimbangul@users.noreply.github.com>
2023-01-15 20:31:22 +0900

    style: media query add

--- components/layout/Header.jsx
+++ components/layout/Header.jsx
@@ -3,6 +3,9 @@ import menu from 'data/menu.json';
 import app from 'data/app.json';
 
 import LOGO from 'src/assets/images/logo.svg';
+import LOGO_ICON from 'src/assets/images/logo_icon.svg';
+import STORE_24 from 'src/assets/images/store_24.svg';
+import INFO_24 from 'src/assets/images/info_24.svg';
 
 // COMPONENT main component
 const Header = () => {
@@ -21,6 +24,7 @@ const HeaderTitle = (props) => {
   return (
     <div className='Header__title'>
       <Link href={props.link || '/'}>
+        <LOGO_ICON className='Header__logo--mb' />
         <LOGO className='Header__logo' />
         <span className='Header__title-text'>{props.title || app.title}</span>
       </Link>
@@ -30,6 +34,14 @@ const HeaderTitle = (props) => {
 
 // COMPONENT header menu list
 const HeaderMenu = () => {
+  const getIcon = (title) => {
+    switch (title) {
+      case 'STORE':
+        return <STORE_24 className='Header__menu-icon' />;
+      case 'ABOUT':
+        return <INFO_24 className='Header__menu-icon' />;
+    }
+  };
   return (
     <nav className='Header__menu'>
       {menu.length > 0 && (
@@ -37,7 +49,10 @@ const HeaderMenu = () => {
           {menu.map((el, idx) => {
             return (
               <li className='Header__menu-item' key={`menuList${idx}`}>
-                <Link href={el.link}>{el.title}</Link>
+                <Link href={el.link}>
+                  {getIcon(el.title)}
+                  <span className='Header__menu-text'>{el.title}</span>
+                </Link>
               </li>
             );
           })}
